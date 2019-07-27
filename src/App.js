ccbeb50304b8e06aa7606806933c5c373a262e50
Katie Rose <katiefelten@gmail.com>
2019-07-27 13:20:17 -0400

    :lipstick: adding homepage styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+//Components
+import HomePage from "./components/homepage/homepage.component";
 
-class App extends Component {
-  render() {
-    return <div className='App'>Hello World</div>;
-  }
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
